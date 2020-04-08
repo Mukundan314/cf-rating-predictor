24d6c6e8b57dc704ec44e01e5b5cd299086bcd44
Mukundan Senthil <mukundan314@gmail.com>
2020-04-08 07:33:57 +0530

    Setup config files

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,57 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
 
 var (
+	cfgFile string
+
 	rootCmd = &cobra.Command{
-		Use:   "cf-rating-predictor [flags]",
+		Use:   "cf-rating-predictor",
 		Short: "Server for cf-rating-predictor",
 		Run:   run,
 	}
 )
 
-func init() {
-}
-
-func run(cmd *cobra.Command, args []string) {
-}
-
 // Execute executes the root command.
 func Execute() error {
 	return rootCmd.Execute()
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cf-rating-predictor.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".cf-rating-predictor")
+	}
+
+	viper.SetEnvPrefix("cf_rating_predictor")
+	viper.AutomaticEnv()
+
+	viper.ReadInConfig()
+
+	viper.WatchConfig()
+}
+
+func run(cmd *cobra.Command, args []string) {
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/cheran-senthil/cf-rating-predictor
 go 1.14
 
 require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.7
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.4.0
 )
